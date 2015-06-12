61be267892ea0baa21b223fab2e54963e165c8b8
EEP <coolicer@foxmail.com>
2015-06-12 16:25:26 +0800

    123

---
+++ kj.js
@@ -0,0 +1,80 @@
+var CronJob    = require('cron').CronJob,
+    cheerio    = require('cheerio'),
+    nodemailer = require('nodemailer'),
+    async      = require('async'),
+    request    = require('request'),
+    gbk        = require('gbk')
+
+var job = new CronJob({
+  cronTime: '00 30 07 * * 2,4,7',
+  onTick: function() {
+      async.waterfall([
+          function(next) {
+              getHTML(next);
+          },
+          function(body,next){
+              parser(body,next)
+          },
+          function(html,next){
+              getResult(html,next)
+          }
+      ], function (err, result) {
+         sendMail(result)
+      });
+  },
+  start: false,
+  timeZone: 'Asia/ShangHai'
+});
+
+job.start();
+
+function getHTML(next) {
+    var options = {
+        url: 'http://kaijiang.500.com/dlt.shtml',  
+        encoding: null
+    }
+    request(options, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            next(null, body)
+          }
+    })
+}
+function parser(body,next) { 
+    var result = gbk.toString('utf-8', body)
+    next(null, result)
+}
+function getResult(html,next) {
+     var $ = cheerio.load(html,{decodeEntities: false})
+     var table = $('table.kj_tablelist02')
+     var series = $('.span_left',table).text()
+     var number = $('.ball_box01 ul', table).text()
+         number = number.replace(/\s+/g, ' ')
+     next(null, {series:series, number:number})
+}
+function sendMail(content,next) {
+  var smtpTransport = nodemailer.createTransport({
+        service: "QQ",
+        auth: {
+            user: "271064579",
+            pass: "eep@liyipiao.com"
+        }
+    });
+
+    //邮件选项设置
+    var mailOptions = {
+        from: "coolicer@foxmail.com", // 发件人地址
+        to: "liyipiao@163.com", //多个收件人用,分隔
+        subject: content['series'], // 主题
+        html: "<h1>" + content['number']+"</h1>"
+    }
+
+    //发送
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent ok");
+        }
+        smtpTransport.close();
+    });
+}
