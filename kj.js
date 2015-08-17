bd0ab3ae97625d62c7cbb7034fa4da98810d6aff
coolicer <coolicer@foxmail.com>
2015-08-17 13:36:30 +0800

    123

--- kj.js
+++ kj.js
@@ -1,26 +1,22 @@
-var CronJob    = require('cron').CronJob,
-    cheerio    = require('cheerio'),
-    nodemailer = require('nodemailer'),
-    async      = require('async'),
-    request    = require('request'),
-    gbk        = require('gbk')
+var waterfall   = require('async').waterfall;
+var iconv = require('iconv-lite');
+var request = require('request');
+var cheerio = require('cheerio');
+var nodemailer = require('nodemailer');
+
+var App = {
+    form: process.env.app_form,
+    to:process.env.app_to,
+    user:process.env.app_user,
+    pass:process.env.app_pass,
+}
+
+var url = 'http://kaijiang.500.com/dlt.shtml';
 
 var job = new CronJob({
   cronTime: '00 30 07 * * 2,4,7',
   onTick: function() {
-      async.waterfall([
-          function(next) {
-              getHTML(next);
-          },
-          function(body,next){
-              parser(body,next)
-          },
-          function(html,next){
-              getResult(html,next)
-          }
-      ], function (err, result) {
-         sendMail(result)
-      });
+    app();
   },
   start: false,
   timeZone: 'Asia/ShangHai'
@@ -28,53 +24,73 @@ var job = new CronJob({
 
 job.start();
 
-function getHTML(next) {
-    var options = {
-        url: 'http://kaijiang.500.com/dlt.shtml',  
+var app = function(){
+	waterfall([
+		function(cb) {
+            getHTML(cb)
+        },
+		function(body,cb){
+            parser(body,cb)
+        },
+		function(html,cb){
+            getResult(html,cb)
+        },
+		function(result){
+			sendMail(result)
+		}
+	])
+}
+
+function getHTML(cb){
+	var options = {
+        url: url,
         encoding: null
     }
-    request(options, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            next(null, body)
-          }
+
+	request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            cb(null, body)
+        }
     })
 }
-function parser(body,next) { 
-    var result = gbk.toString('utf-8', body)
-    next(null, result)
+function parser(body,cb){
+	var result = iconv.decode(body, 'GBK');
+	cb(null, result)
 }
-function getResult(html,next) {
-     var $ = cheerio.load(html,{decodeEntities: false})
-     var table = $('table.kj_tablelist02')
-     var series = $('.span_left',table).text()
-     var number = $('.ball_box01 ul', table).text()
-         number = number.replace(/\s+/g, ' ')
-     next(null, {series:series, number:number})
+function getResult(html,cb) {
+     var $ = cheerio.load(html,{decodeEntities: false});
+     var table = $('table.kj_tablelist02');
+     var series = $('.span_left',table).text();
+     var number = $('.ball_box01 ul', table).text();
+         number = number.replace(/\s+/g, ' ');
+         number = number.trim().split(' ');
+         number.splice(5,0,'-');
+         number = number.join(' ');
+     cb(null, {series:series, number:number})
 }
-function sendMail(content,next) {
+function sendMail(content) {
   var smtpTransport = nodemailer.createTransport({
         service: "QQ",
         auth: {
-            user: "YOUR QQ",
-            pass: "YOUR QQ Password"
+            user: App.user,
+            pass: App.pass
         }
     });
 
-    //邮件选项设置
     var mailOptions = {
-        from: "", // 发件人地址
-        to: "", //多个收件人用,分隔
-        subject: content['series'], // 主题
+        from: App.form,
+        to: App.to,
+        subject: content['series'],
         html: "<h1>" + content['number']+"</h1>"
     }
 
-    //发送
     smtpTransport.sendMail(mailOptions, function(error, response){
         if(error){
             console.log(error);
         }else{
-            console.log("Message sent ok");
+            console.log('ok!');
         }
         smtpTransport.close();
     });
 }
+module.exports = app
